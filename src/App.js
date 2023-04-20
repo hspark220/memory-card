acfe101888656b1ae88650818843c13e8eeed3b0
Heon Soo Park <hspark220@gmail.com>
2023-04-20 08:21:55 -0400

    Create number randomizer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ const App = () => {
   const [cards, setCard] = useState([1,2,3,4]);
   const [usedCards, setUsedCards] = useState([]);
 
+  const getRandomNumberArray = () => {
+    const array = [];
+    let randomNumber = Math.floor(Math.random()*10);
+    for (let i = 0; i < 4; i++) {
+      while (array.includes(randomNumber)) randomNumber = Math.floor(Math.random()*10);
+      array.push(randomNumber)
+    }
+    return array;
+  }
+
   const incrementScore = e => {
     const value = e.target.id;
     
@@ -17,6 +27,8 @@ const App = () => {
       setScore(0);
     } else {
       setUsedCards(usedCards.concat(value));
+      
+      setCard(getRandomNumberArray)
       setScore(score + 1);
     }
     
